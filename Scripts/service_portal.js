7b2f2c661ceaed0cfc49457e5cfc1deee3d77563
MateusOrganize <72801158+MateusOrganize@users.noreply.github.com>
2020-12-10 09:48:14 -0300

    BRL

--- Scripts/service_portal.js
+++ Scripts/service_portal.js
@@ -233,3 +233,15 @@ c.convertNumber = function(n){
 		return aux + ',' + n.slice(5,8) + " M";
 	}
 }
+
+/* ---------  Format Numbers to 'BRL' --------- */
+var atual = 600000.00;
+
+//com R$
+var f = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+//sem R$
+var f2 = atual.toLocaleString('pt-br', {minimumFractionDigits: 2});
+
+console.log(f);
+console.log(f2);
