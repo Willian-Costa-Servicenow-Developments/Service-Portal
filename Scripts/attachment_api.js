808b4fff824a5b5cde852a13269de720e2851430
WillianCostaOCL <72630435+WillianCostaOCL@users.noreply.github.com>
2020-12-17 11:25:39 -0300

    Update attachment_api.js

--- Scripts/attachment_api.js
+++ Scripts/attachment_api.js
@@ -28,56 +28,56 @@ data.instanceID = instance.getValue('sp_widget');
  */
  
  $scope.uploadFiles = function() {	
-    // Convert to Base64 for Auth
-		var base64User = btoa(c.data.user + ':' + c.data.password);
-   
-   //Get File
-		var input = document.getElementById('upload_file');
-		$scope.files = input.files;
-  
-    // Serialize to FormData
-		$scope.fd = new FormData();
-		var file = $scope.files[0];
-		$scope.fd.set('files', file);
-    
-    // Configure Communication 
-		var request = {
-			method: 'POST',
-			url: 'https://'+ c.data.instance_name +'.service-now.com/api/now/attachment/file?table_name='+c.data.table+'&table_sys_id='+c.data.sys_id+'&file_name='+file.name,
-			data: $scope.fd.get('files'),
-			headers: {
-				'Content-Type': file.type,
-				'Accept':'application/json',
-				'Authorization': 'Basic ' + base64User
-			}
-		};
-		
-    console.log('HTTP request:',request);
+	// Convert to Base64 for Auth
+	var base64User = btoa(c.data.user + ':' + c.data.password);
 
-		// SEND THE FILES.
-		$http(request)
-			.success(function (d) {
-        $timeout(function(){	
-          c.logoutUSer();
-        });
-		  })
-      .error(function (err) {
-        console.log(err);
-       });
+	//Get File
+	var input = document.getElementById('upload_file');
+	$scope.files = input.files;
+
+	// Serialize to FormData
+	$scope.fd = new FormData();
+	var file = $scope.files[0];
+	$scope.fd.set('files', file);
+    
+    	// Configure Communication 
+	var request = {
+		method: 'POST',
+		url: 'https://'+ c.data.instance_name +'.service-now.com/api/now/attachment/file?table_name='+c.data.table+'&table_sys_id='+c.data.sys_id+'&file_name='+file.name,
+		data: $scope.fd.get('files'),
+		headers: {
+			'Content-Type': file.type,
+			'Accept':'application/json',
+			'Authorization': 'Basic ' + base64User
+		}
+	};
+		
+	console.log('HTTP request:',request);
+
+	// SEND THE FILES.
+	$http(request)
+	.success(function (d) {
+		$timeout(function(){	
+			c.logoutUSer();
+		});
+	})
+	.error(function (err) {
+		console.log(err);
+	});
 
 
 	}
 
-  // This function ensures that the authenticated user does not open a session
+	// This function ensures that the authenticated user does not open a session
 	c.logoutUSer = function(){
 		var request = {
 			url: 'https://'+ c.data.instance_name +'.service-now.com/logout.do'
 		};
 		$http(request)
-			.success(function (d) {
-        $timeout(function(){	
-          console.log("it's disconnected");
-        });
+		.success(function (d) {
+			$timeout(function(){	
+				console.log("it's disconnected");
+			});
 		});
 	}
 
